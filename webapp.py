6937e346401700fe57c3f686554927cffb4f1eb6
f.c.lopez <casmali.f.lopez@gmail.com>
2018-02-23 09:26:29 -0800

    Update webapp.py

--- webapp.py
+++ webapp.py
@@ -63,6 +63,7 @@ def posts_to_html():
                 ret += Markup("<tr> <td>" + i[0] +  "</td> <td>" +i[1] + "</td></tr>") 
                 print("<tr> <td>" + i[0] +  "</td> <td>" +i[1] + "</td></tr>")
                 print(i[0])
+                print(i)
     except:
         print("error")
     ret += Markup("</table>")
